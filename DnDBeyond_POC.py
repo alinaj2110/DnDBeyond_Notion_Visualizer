a9cd7589fe036b7f8ae6ca1efb07c4f06ce101f5
alinaj2110 <anjalipraveen2110@gmail.com>
2024-04-09 14:52:36 +0530

    Added stealth for headless browsing and changed window size

--- DnDBeyond_POC.py
+++ DnDBeyond_POC.py
@@ -1,6 +1,7 @@
 import asyncio
 from pyppeteer import launch
 from core.utils import *
+from pyppeteer_stealth import stealth
 
 #char_link = "https://www.dndbeyond.com/characters/76483006"
 char_link = "https://www.dndbeyond.com/characters/120033071/U6lo2s"
@@ -11,18 +12,18 @@ async def main():
     if not exec_path:
         print("No Chrome Executable found")
         return
-    browser = await launch(executablePath= exec_path, headless=False, defaultViewport=None, args=['--start-fullscreen'])  # Set headless to True if you don't want to see the browser window
-
+    browser = await launch(executablePath= exec_path, headless=True, defaultViewport=None, args=['--window-size=1920,1080'])
     # Create a new page
     page = await browser.newPage()
-
+    await stealth(page)
     # Navigate to a website
     await page.goto(char_link)
     await asyncio.sleep(5)
 
     combat_tags = ["#character-tools-target","ct-character-sheet__inner",
                     "ct-subsections", "ct-subsection ct-subsection--primary-box",
-                    "ddbc-tab-list","ddbc-tab-options__content"]#,"ct-actions-list"]
+                    "ddbc-tab-list","ddbc-tab-options__content"]
+    # #ct-actions-list__heading
     element = await get_element(combat_tags, page)
     await highlight_element(element, page)
     await remove_highlight(element, page)
