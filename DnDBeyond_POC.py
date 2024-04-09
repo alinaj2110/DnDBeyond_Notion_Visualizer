15aac69a53827b7db7d7fd519bdfdde550f8503a
alinaj2110 <anjalipraveen2110@gmail.com>
2024-04-09 15:45:15 +0530

    headless True or False based on debug mode

--- DnDBeyond_POC.py
+++ DnDBeyond_POC.py
@@ -1,3 +1,4 @@
+import argparse
 import asyncio
 from pyppeteer import launch
 from core.utils import *
@@ -7,12 +8,25 @@ from pyppeteer_stealth import stealth
 char_link = "https://www.dndbeyond.com/characters/120033071/U6lo2s"
 
 async def main():
+    parser = argparse.ArgumentParser(description='Description of your program')
+    
+    # Add arguments
+    parser.add_argument('--debug', action='store_true', help='Debug Mode')
+    
+    # Parse the arguments
+    args = parser.parse_args()
+    
+    # Access the parsed arguments
+    debug_mode = args.debug
+
     # Launch the browser
     exec_path = find_chrome_executable()
     if not exec_path:
         print("No Chrome Executable found")
         return
-    browser = await launch(executablePath= exec_path, headless=True, defaultViewport=None, args=['--window-size=1920,1080'])
+
+    headless = False if debug_mode else True
+    browser = await launch(executablePath= exec_path, headless=headless, defaultViewport=None, args=['--window-size=1920,1080'])
     # Create a new page
     page = await browser.newPage()
     await stealth(page)
@@ -32,7 +46,7 @@ async def main():
     combat_action_list = await element.querySelectorAll(".ct-actions-list")
     for comb_action in combat_action_list:
         await highlight_element(comb_action, page)
-        text = await get_text_content(element, page)
+        text = await get_text_content(comb_action, page)
         await remove_highlight(comb_action, page)
 
     # Close the browser
