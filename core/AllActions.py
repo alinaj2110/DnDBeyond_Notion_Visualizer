3cf69ef87af827060b16e28c96e8c84f87e7cfa2
alinaj2110 <anjalipraveen2110@gmail.com>
2024-04-10 16:14:02 +0530

    Added Section parent class for Action, Bonus Action and reaction classes

--- core/AllActions.py
+++ core/AllActions.py
@@ -32,46 +32,87 @@ class AllActions:
             match typ:
                 case C_Actions.ACTION:
                     self.actions = Action(comb_action)
+                    await self.actions.parse_contents()
                 case C_Actions.BONUS_ACTION:
                     self.bonus_actions = BonusAction(comb_action)
+                    await self.bonus_actions.parse_contents()
                 case C_Actions.REACTION:
                     self.reactions = Reactions(comb_action)
+                    await self.reactions.parse_contents()
                 case C_Actions.OTHER:
                     self.others = comb_action
 
-class Action:
+class Section:
     def __init__(self, stats) -> None:
-        self.action_element = stats
+        self.stats_element = stats
     
-    async def parse_actions(self):
-        pass
-
-    def add_to_action(self):
+    async def parse_contents(self):
         pass
     
-
-class BonusAction:
-    def __init__(self, stats) -> None:
-        self.bonus_stats = stats
-
-    async def parse_bonus_actions(self):
+    def add_other_content(self):
         pass
 
-    def add_to_bonus_actions(self):
-        pass
+class Action(Section): 
+    async def parse_contents(self):
+        '''
+        ct-actions-list
+        |-ct-actions-list__heading
+            |-ct-actions__attacks-heading  -- for action and attack #
+        |-ct-actions-list__content
+            |- ddbc-attack-table
+                |-ddbc-attack-table__row-header
+                |ddbc-attack-table__content
+                    |-ddbc-combat-attack__name
+            |-ct-actions-list__basic
+            |-ct-actions-list__activatable [list]
+                |-ct-feature-snippet
+        '''
+        # if shared_data.debug_enabled : await highlight_element(self.action_element)
+        print("THE ACTION CLASS IS PARSING")
 
-class Reactions:
-    def __init__(self, stats) -> None:
-        self.react_stats = stats
-   
-    async def parse_reactions(self):
-        pass
+    
 
-    def add_to_reactions(self):
-        pass
+class BonusAction(Section):
+    async def parse_contents(self):
+        '''
+        ct-actions-list
+        |-ct-actions-list__heading
+            |-ct-actions__attacks-heading 
+        |-ct-actions-list__content
+            |-ct-actions-list__basic
+            |-ct-actions-list__activatable [list]
+                |-ct-feature-snippet
+        '''
+        print("THE BONUS ACTION CLASS IS PARSING")
+
+
+class Reactions(Section):
+    async def parse_contents(self):
+        '''
+        ct-actions-list
+        |-ct-actions-list__heading
+            |-ct-actions__attacks-heading 
+        |-ct-actions-list__content
+            |-ct-actions-list__basic
+            |-ct-actions-list__activatable [list]
+                |-ct-feature-snippet
+        '''
+        print("THE REACTION CLASS IS PARSING")
+
 
 class Spells:
     def __init__(self, spellblock) -> None:
         self.spellblock = spellblock
 
         
+'''
+Others:
+ct-actions-list
+    |-ct-actions-list__heading
+        |-ct-actions__attacks-heading 
+    |-ct-actions-list__content
+        |-ct-actions-list__basic
+        |-ct-actions-list__activatable [list]
+            |-ct-feature-snippet
+
+'''
